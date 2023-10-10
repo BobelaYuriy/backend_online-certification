0c32ddf61d2b9f7087f158904e43e9cd19fe3466
BobelaYuriy <yra.bobela@gmail.com>
2023-10-10 17:43:09 +0300

    test

--- routes/courses.js
+++ routes/courses.js
@@ -1,12 +1,12 @@
 var express = require('express');
 var router = express.Router();
 const CardsUsers = require('../models/cards')
-
+const arr = [1,2,3,4,5]
 router.get('/courses', async (req, res) => {
     try {
       // Отримуємо дані з колекції
-      const cardsUsers = await CardsUsers.find();
-      res.json(cardsUsers);
+      //const cardsUsers = await CardsUsers.find();
+      res.json(arr);
     } catch (error) {
       res.status(500).json({ error: error.message });
     }
