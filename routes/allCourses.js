7a0b9ba4a343c0dbfb1e03524cd67a7dce594125
BobelaYuriy <yra.bobela@gmail.com>
2023-10-11 11:37:07 +0300

    add token

--- routes/allCourses.js
+++ routes/allCourses.js
@@ -1,11 +1,31 @@
 const express = require('express');
 const db = require('../db');
+const jwt = require('jsonwebtoken');
 const CardsUsers = require('../models/cards')
 
 const router = express.Router();
 const SECRET = process.env.JWT_SECRET;
 
-router.get('/courses', async (req, res) => {
+function verifyToken(req, res, next) {
+    const token = req.headers.authorization;
+
+    if (!token) {
+        return res.status(401).json({ error: 'Authorization token is missing' });
+    }
+
+    jwt.verify(token.split(' ')[1], SECRET, (err, decoded) => {
+        console.log('Decoded token:', decoded);
+        if (err) {
+            return res.status(401).json({ error: 'Invalid token' });
+        }
+
+        req.decodedToken = decoded;
+
+        next();
+    });
+}
+
+router.get('/courses',verifyToken, async (req, res) => {
     try {
         const cardsUsers = await CardsUsers.find();
     
