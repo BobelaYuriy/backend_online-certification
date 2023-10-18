5bef4de1058df5c5f24657421819414d7398cd84
BobelaYuriy <yra.bobela@gmail.com>
2023-10-18 22:22:56 +0300

    test

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -82,7 +82,6 @@ const signin = async (req, res, next) => {
         await tokenService.saveToken(userDto.id, tokens.refreshToken);
         const userData = {...tokens, user: userDto};
 
-        res.cookie('refreshToken', userData.refreshToken, { maxAge: 30 * 24 * 60 * 60 * 1000, httpOnly: true });
         res.status(200).json(userData);
         
     } catch (err) {
