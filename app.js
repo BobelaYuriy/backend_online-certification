31e5048aeaf41c3a69822cc0219dd21b7ca60df6
BobelaYuriy <yra.bobela@gmail.com>
2023-10-19 13:09:20 +0300

    test

--- app.js
+++ app.js
@@ -21,8 +21,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(cors({
-  credentials: true,
-  origin: process.env.CLIENT_URL
+  credentials: true
 }));
 
 //роути
