1cb4c2936a69d012c32020a077dbb93d345118b4
BobelaYuriy <yra.bobela@gmail.com>
2023-10-19 14:50:27 +0300

    має робити надiюсь

--- models/token.js
+++ models/token.js
@@ -1,9 +1,9 @@
 const mongoose = require('mongoose')
 
 const TokenSchema = new mongoose.Schema({
-user:{type:mongoose.Schema.ObjectId, ref:'user'},
+user:{type:mongoose.Schema.ObjectId, ref:'users'},
 refreshToken:{type:String, required:true},
 })
 
-const token = mongoose.model('users', TokenSchema)
+const token = mongoose.model('tokens', TokenSchema)
 module.exports = token;
