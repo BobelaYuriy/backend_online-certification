e994e2333eb049c953ddadc03cf239cbfab25235
BobelaYuriy <yra.bobela@gmail.com>
2023-10-21 16:18:05 +0300

    update refresh

--- services/token-service.js
+++ services/token-service.js
@@ -3,7 +3,7 @@ const tokenModel = require('../models/token');
 
 class TokenService {
     generateTokens(payload) {
-        const accessToken = jwt.sign(payload, process.env.JWT_ACCESS_SECRET, {expiresIn: '2h'})
+        const accessToken = jwt.sign(payload, process.env.JWT_ACCESS_SECRET, {expiresIn: '15s'})
         const refreshToken = jwt.sign(payload, process.env.JWT_REFRESH_SECRET, {expiresIn: '30s'})
         return {
             accessToken,
