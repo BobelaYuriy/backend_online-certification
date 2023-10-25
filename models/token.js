a983f0016162a8a3bd87c478954e9c77013b89d7
BobelaYuriy <yra.bobela@gmail.com>
2023-10-25 15:37:50 +0300

    test

--- models/token.js
+++ models/token.js
@@ -1,6 +1,6 @@
 const {Schema, model} = require('mongoose');
 
-const TokenSchema = new mongoose.Schema({
+const TokenSchema = new Schema({
     user:{type: Schema.Types.ObjectId, ref:'users'},
     refreshToken:{type:String, required:true},
 })
